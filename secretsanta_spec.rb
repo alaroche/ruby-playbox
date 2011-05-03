c9eb592527b5395f02ade66f5ddacc50439d9812
Aaron LaRoche <alaroche22@gmail.com>
2011-05-02 23:12:08 -0400

    Skeleton of a test.

---
+++ secretsanta_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe SecretSanta do
+  before(:each) do
+    secret_santa = SecretSanta.run
+  end
+
+  it 'should return an array of hashes'
+
+  it 'should not have santa the same as receiver.'
+
+  it 'should have each person as both a santa and a reciever'
+
+end
